81f88b1390d8b84a2f7ee27a9834848982910a6d
rash <rashdan.developer@gmail.com>
2022-12-15 15:44:47 +0000

    user endpoints

--- src/main/java/us/group14/backend/user/UserController.java
+++ src/main/java/us/group14/backend/user/UserController.java
@@ -3,23 +3,44 @@ package us.group14.backend.user;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import us.group14.backend.annotations.ApiMapping;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
-@ApiMapping("/users")
+@ApiMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+
 
     @GetMapping
     @JsonView(UserView.class)
     public ResponseEntity<List<User>> getAllUsers() {
         return userService.getAllUsers();
     }
+
+    @PostMapping("/contacts/{id}")
+    public void addContact(AuthUser user, @PathVariable("id") Long id) {
+        userService.addContact(user.get(), id);
+    }
+
+    @GetMapping("/contacts")
+    public Set<User> getContacts(AuthUser user){
+        return userService.getContacts(user.get());
+    }
+
+    @DeleteMapping("/contacts/{id}")
+    public String deleteContact(AuthUser user, @PathVariable("id") Long id) {
+        userService.deleteContact(user.get(), id);
+//        AuthUser deleteContact = authUserRepository.findById(id).get();
+//        authUserRepository.delete(deleteContact);
+//        return "Contact deleted";
+    }
+
+
 
 }
