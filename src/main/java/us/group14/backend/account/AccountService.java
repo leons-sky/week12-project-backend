8b2eca7d895bb94d7defb38405eefa32b9cbd032
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2022-12-15 11:40:55 +0000

    Create account controller and service

--- src/main/java/us/group14/backend/account/AccountService.java
+++ src/main/java/us/group14/backend/account/AccountService.java
@@ -1,2 +1,9 @@
-package us.group14.backend.account;public class AccountService {
+package us.group14.backend.account;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AccountService {
 }
