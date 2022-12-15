262e2f6b9cc9003dcd1bc7027a78e2967a85608a
Ben Stirling <benjjstirling@gmail.com>
2022-12-15 15:52:03 +0000

    Created models & Repositories

---
+++ src/main/java/us/group14/backend/account/Account.java
@@ -0,0 +1,62 @@
+package us.group14.backend.account;
+
+import jakarta.persistence.*;
+import us.group14.backend.transaction.Transaction;
+import us.group14.backend.user.User;
+
+import java.util.Set;
+
+@Entity
+public class Account {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    private Double balance;
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "Transaction")
+    private Set<Transaction> transactions;
+
+    public Set<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(Set<Transaction>transactions) {
+        transactions = transactions;
+    }
+
+   public void withdraw (double amount) {
+        this.balance = this.balance - amount;
+   }
+
+    public void deposit (double amount) {
+        this.balance = this.balance + amount;
+    }
+}
---
+++ src/main/java/us/group14/backend/transaction/Transaction.java
@@ -0,0 +1,86 @@
+package us.group14.backend.transaction;
+
+import jakarta.persistence.*;
+import jakarta.persistence.spi.PersistenceUnitTransactionType;
+import org.bouncycastle.cms.Recipient;
+import us.group14.backend.account.Account;
+import us.group14.backend.user.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Transaction {
+
+    private Long id;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private Double amount;
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    private LocalDateTime transferredAt;
+
+    public LocalDateTime getTransferredAt() {
+        return transferredAt;
+    }
+
+    public void setTransferredAt(LocalDateTime transferredAt) {
+        this.transferredAt = transferredAt;
+    }
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "recipient")
+    private Account recipient;
+
+    public Account getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(Account recipient) {
+        this.recipient = recipient;
+    }
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "sender")
+    private Account sender;
+
+    public Account getSender() {
+        return sender;
+    }
+
+    public void setSender(Account sender) {
+        this.sender = sender;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    private TransactionType type;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private String message;
+}
