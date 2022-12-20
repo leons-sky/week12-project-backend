18d9b5b02f71657216aaa1086487f001637745c8
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2022-12-20 16:32:45 +0000

    Validation exception handles

---
+++ src/main/java/us/group14/backend/advice/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,51 @@
+package us.group14.backend.advice;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.boot.json.JsonParseException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler
+        extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Throwable cause = ex.getCause();
+        if (cause instanceof InvalidFormatException) {
+            InvalidFormatException fex = (InvalidFormatException) cause;
+
+            Map<String, String> errors = new HashMap<>();
+            String fieldName = fex.getPath().get(0).getFieldName();
+            Object fieldValue = fex.getValue();
+            String errorMessage = String.format("Invalid value '%s' of type '%s' expected a valid '%s'", fieldValue, fieldValue.getClass().getSimpleName(), fex.getTargetType().getSimpleName());
+            errors.put(fieldName, errorMessage);
+            return handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        }
+        return super.handleHttpMessageNotReadable(ex, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            System.out.println(error);
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}
