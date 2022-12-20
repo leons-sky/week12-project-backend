da264a471d627c4e90fb1a130afafccbabebf903
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2022-12-20 15:15:18 +0000

    Fix transfer()

--- src/main/java/us/group14/backend/transaction/TransactionRequest.java
+++ src/main/java/us/group14/backend/transaction/TransactionRequest.java
@@ -1,16 +1,26 @@
 package us.group14.backend.transaction;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @ToString
 @AllArgsConstructor
 public class TransactionRequest {
 
+    @NotNull(message = "Amount is required")
+    @PositiveOrZero
     private final Double amount;
+
+    @Length(max = 255)
     private final String message;
-    private final Long recipientId;
+
+    private final String recipient;
 
 }
