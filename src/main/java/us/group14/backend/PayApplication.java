a0c39370fa310be400c5f1af1f3b1280625680dc
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2022-12-21 12:47:11 +0000

    Add application context

--- src/main/java/us/group14/backend/PayApplication.java
+++ src/main/java/us/group14/backend/PayApplication.java
@@ -3,8 +3,10 @@ package us.group14.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@ImportResource({"classpath*:application-context.xml"})
 public class PayApplication {
 
 	public static void main(String[] args) {
